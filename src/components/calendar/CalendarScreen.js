4cf1ff3cc4061ff28f37617074ab25f457978dc4
Daniel Pinelli <danielpinelli.fullstack@gmail.com>
2022-05-20 13:44:00 -0300

    Calendar working

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,10 +1,31 @@
-import Navbar from "../ui/Navbar"
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment'
 
+import Navbar from "../ui/Navbar"
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+const localizer = momentLocalizer(moment)
+
+const myEventsList = [
+    {
+        title: 'All Day Event very long title',
+        start: moment().toDate(),
+        end: moment().add(2, 'hours').toDate(),
+        bgcolor: '#fafafa',
+    }
+]
 
 const CalendarScreen = () => {
     return (
-        <div>
+        <div className='calendar-screen' >
             <Navbar />
+            <Calendar
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 500 }}
+            />
         </div>
     )
 }
--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -8,7 +8,8 @@ const Navbar = () => {
             </span>
 
             <button className='btn btn-outline-danger' >
-
+                <i className='fas fa-sign-out-alt'></i>}
+                <span>  Sign Out</span>
             </button>
 
         </div>
